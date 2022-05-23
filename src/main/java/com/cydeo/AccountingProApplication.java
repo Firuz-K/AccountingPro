0871ca797cbd65857685ff7f8d9b011e4f33e7ac
myizci <izci@hotmail.com>
2022-05-22 22:31:33 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/AccountingProApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingProApplication.class, args);
+    }
+
+}
