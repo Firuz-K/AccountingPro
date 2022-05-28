5ca41ad7b4960563645b1591592cf30328cf6ab5
Firuz Khalimov <fkhalimov22@gmail.com>
2022-05-29 00:57:58 +0500

    Removed name and description fields

--- src/main/java/com/applicationpro/entity/Stock.java
+++ src/main/java/com/applicationpro/entity/Stock.java
@@ -14,14 +14,13 @@ import java.math.BigDecimal;
 @Where(clause = "is_deleted=false")
 public class Stock extends BaseEntity{
 
-    private String name;
-    private String description;
+
     private int quantity;
     private int remainingQuantity;
     private BigDecimal price;
 
     @OneToOne
-    @JoinColumn(name = "product_d")
+    @JoinColumn(name = "product_id")
     private Product product;
 
 
