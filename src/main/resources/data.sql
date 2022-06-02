ffd83dae8885e13e6dccd79ab50a4318d274199b
Sergiu Tcaci <afon2002@gmail.com>
2022-06-01 21:26:45 -0500

    Removed AllArgConst

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,3 @@
-insert into users(id, created_by, created_time, is_deleted, updated_by, updated_time, email, enabled, first_name, last_name, password, phone ,company_id, role_id)
-values(1, 1, '2021-01-05 00:00:00', false, 1, '2021-01-15 00:00:00', 'admin@admin.com', true, 'Mike', 'Smith', null, '7894561231',null, null),
-        (2, 2, '2021-01-06 00:00:00', false, 2, '2021-01-15 00:00:00', 'manager@admin.com', true, 'Mikey', 'Smith', null, '7894561230',null, null),
-        (3, 2, '2021-01-07 00:00:00', false, 3, '2021-01-15 00:00:00', 'employee@admin.com', true, 'Micheal', 'Smith', null, '7894561235',null, null);
-
-INSERT INTO roles(enabled, name)
-values(true, 'Admin'),
-      (true, 'Manager'),
-      (true, 'Employee'),
-      (true, 'Root');
 insert into users(id, created_by, created_time, is_deleted, updated_by, updated_time, email, enabled, first_name,
                   last_name, password, phone, company_id, role_id)
 values (1, 1, '2021-01-05 00:00:00', false, 1, '2021-01-15 00:00:00', 'admin@admin.com', true, 'Mike', 'Smith', null,
@@ -28,3 +18,9 @@ VALUES (1, 1, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', '3073 Sumn
         true, '2020-01-05', '484-556-6097', 'John Unilever', 'PA', 'Unilever', '19714'),
        (4, 1, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', '1116 Cooks Mine Road', '', 'abc@bp.com',
         true, '2020-01-05', '435-215-6440', 'John BP', 'UT', 'BP', '84104');
+
+INSERT INTO roles(enabled, name)
+values(true, 'Admin'),
+      (true, 'Manager'),
+      (true, 'Employee'),
+      (true, 'Root');
