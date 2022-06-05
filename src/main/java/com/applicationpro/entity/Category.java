18c82db0049f493e2b0b0e3e28df135d4b1a2178
Filizsi <filizcoskunuzer@gmail.com>
2022-06-05 11:17:36 -0500

    update Category services update main2, category-add,category-list,category-edit html documents create Category controller update categoryDTO added Category data to data.sgl file

--- src/main/java/com/applicationpro/entity/Category.java
+++ src/main/java/com/applicationpro/entity/Category.java
@@ -8,34 +8,23 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Table(name = "categories") // should we keep the name as is?
+@Table(name = "categories")
 @NoArgsConstructor
 @Getter
 @Setter
 @Where(clause = "is_deleted=false")
-// do we need all arg constructor?
 public class Category extends BaseEntity {
 
     private String description;
     private Boolean enabled;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "company_id")
-    private Company company; // I made the return type a list , omer
+//    @ManyToMany(fetch = FetchType.LAZY)
+//    @JoinTable(name = "category_company_rel",
+//            joinColumns = @JoinColumn(name="category_id"),
+//            inverseJoinColumns = @JoinColumn(name = "company_id"))
+//    private List<Company> companies;
+
+    @ManyToOne (fetch = FetchType.LAZY)
+    private Company company;
+
 }
-
-/*
-@NoArgsConstructor
-@Data
-@Entity
-@Where(clause = "is_deleted = false")
-@Table(name = "categories")
-public class Category extends BaseEntity{
-
-
-    private String description;
-    @ManyToOne //should we use the fetch lazy --- why we do not use the many to many ...
-    @JoinColumn(name = "company_id")
-    private Company company; //private  List<Company> company;
-    private Boolean enabled;
- */
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,13 +8,27 @@ values (1, 1, '2021-01-05 00:00:00', false, 1, '2021-01-15 00:00:00', 'admin@adm
         null, '7894561235', null, null);
 
 -- Company sample data
-insert into companies(id, created_by, created_time, is_deleted, updated_by, updated_time, address1, address2, email,
+insert into companies( created_by, created_time, is_deleted, updated_by, updated_time, address1, address2, email,
                       enabled, establishment_date, phone, representative, state, title, zip)
-VALUES (1, 1, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', '3073 Sumner Street', '', 'abc@fuji.com', true,
-        '2020-01-05', '310-628-7886', 'John Fuji', 'CA', 'Fuji Holding', '91752'),
-       (2, 1, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', '308 Edwards Street', '', 'abc@hp.com', true,
-        '2020-01-05', '252-794-1258', 'John HP', 'NY', 'HP', '27983'),
-       (3, 1, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', '3868 Franklee Lane', '', 'abc@unilever.com',
-        true, '2020-01-05', '484-556-6097', 'John Unilever', 'PA', 'Unilever', '19714'),
-       (4, 1, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', '1116 Cooks Mine Road', '', 'abc@bp.com',
-        true, '2020-01-05', '435-215-6440', 'John BP', 'UT', 'BP', '84104');
+VALUES (1, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', '3073 Sumner Street', '', 'abc@fuji.com', true,
+        '2020-01-05', '310-628-7886', 'John Fuji', 'CALIFORNIA', 'Fuji Holding', '91752');
+--        (2, 1, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', '308 Edwards Street', '', 'abc@hp.com', true,
+--         '2020-01-05', '252-794-1258', 'John HP', 'NY', 'HP', '27983'),
+--        (3, 1, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', '3868 Franklee Lane', '', 'abc@unilever.com',
+--         true, '2020-01-05', '484-556-6097', 'John Unilever', 'PA', 'Unilever', '19714'),
+--        (4, 1, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', '1116 Cooks Mine Road', '', 'abc@bp.com',
+--         true, '2020-01-05', '435-215-6440', 'John BP', 'UT', 'BP', '84104');
+
+INSERT INTO roles(enabled, name)
+values(true, 'Admin'),
+      (true, 'Manager'),
+      (true, 'Employee'),
+      (true, 'Root');
+
+insert into categories(created_by, created_time, is_deleted, updated_by, updated_time, description, enabled,company_id)
+values (1, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', 'Furniture', true,1) ,
+(1, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', 'Phone', true,1),
+(2, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', 'Clothing', true,1),
+(3, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', 'Electronics', true,1),
+(2, '2021-01-05 00:00:00', false, 1, '2021-01-05 00:00:00', 'Appliances', true,1);
+
