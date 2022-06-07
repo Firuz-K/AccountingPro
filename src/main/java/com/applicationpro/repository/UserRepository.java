6b0a8cdaed4e6e3c9d6183daeb6a6a2e11ac0585
Sergiu Tcaci <afon2002@gmail.com>
2022-06-06 19:04:58 -0500

    UserDTO/Service

---
+++ src/main/java/com/applicationpro/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.applicationpro.repository;
+
+import com.applicationpro.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+}
---
+++ src/main/java/com/applicationpro/service/UserService.java
@@ -0,0 +1,8 @@
+package com.applicationpro.service;
+
+import com.applicationpro.dto.UserDTO;
+import java.util.List;
+
+public interface UserService {
+    List<UserDTO> listAll();
+}
