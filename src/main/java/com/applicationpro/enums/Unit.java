47276d28bc8f40d6ffa104dab0453587676d8a2f
arifsalamci <arifsalamci@gmail.com>
2022-06-07 19:07:28 -0500

    Unit enum is completed

--- src/main/java/com/applicationpro/enums/Unit.java
+++ src/main/java/com/applicationpro/enums/Unit.java
@@ -1,4 +1,16 @@
 package com.applicationpro.enums;
 
 public enum Unit {
+
+    LB("Pound"), PC("Piece");
+
+    private final String value;
+
+    Unit(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
